436de7f5a6a2c40d539ac22f3585a28064a86a65
pritanjanhp <pritanjanbaranwal03.hp@gmail.com>
2025-01-27 10:52:21 +0530

    firebase

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,19 @@
-import { NextConfig } from "next";
+// import { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   output: "export",
+//   images: {
+//     unoptimized: true
+//   }
+// };
+
+// export default nextConfig;
+
+
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    unoptimized: true
-  }
+  /* config options here */
 };
 
 export default nextConfig;
